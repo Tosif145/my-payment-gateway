a9909e30d4cd2d28c4579502e72f38258a9c8e0c
Tosif145 <tklearning2000@gmail.com>
2024-07-17 17:45:28 +0530

    swagger update

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger/swagger.json'); // Make sure the path is correct
+const swaggerDocument = require('./swagger/swagger.json'); // Path to your Swagger JSON file
+const swaggerUiDist = require('swagger-ui-dist');
+const path = require('path');
 const config = require('./config/config');
+
 require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
@@ -35,6 +38,10 @@ app.use('/api/refunds', refundRoutes);
 
 // Serve Swagger UI documentation
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Serve Swagger UI Dist files
+const swaggerUiPath = path.dirname(require.resolve('swagger-ui-dist'));
+app.use('/swagger-ui', express.static(swaggerUiPath));
 
 // Start the server
 const PORT = process.env.PORT || 5000;
